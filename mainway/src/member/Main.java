bd98ecb229aaa25a3b20075dd9112a3c7696afac
namjihun <namjihun1@gmail.com>
2023-04-04 10:29:40 +0900

    변한거 없음

--- mainway/src/member/Main.java
+++ mainway/src/member/Main.java
@@ -7,7 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
+	//로그인 화면 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("loginForm2.fxml")); //loader의 객체 생성, loginForm을 얻어온다
@@ -18,17 +19,14 @@ public class Main extends Application {
 		
 		LoginController loginCon = loader.getController(); // LoginController 객체 생성
 		loginCon.Opener(opener); //opener룰 loginCon에 ?
-		
 		Scene scene = new Scene(loginForm2); //scenebuilder 출력
 		primaryStage.setTitle("로그인 화면"); //화면 이름
 		primaryStage.setScene(scene); //scenebuilder 출력
 		primaryStage.show(); //scenebuilder 출력
 		
-		System.out.println("test");
-		
-	}	
+	}
 	
 	public static void main(String[] args) {
-		launch(args); //scenebuilder 실행
+		launch(args);
 	}
 }
