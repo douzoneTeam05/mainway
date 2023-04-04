ad061aa4ff0ea440efd6a4f49eeec759c7c15227
namjihun <namjihun1@gmail.com>
2023-04-04 09:53:31 +0900

    아아디 찾기 idsearch와 비밀번호 찾기 pwsearch를 구현

---
+++ mainway/src/member/IdSearchDTO.java
@@ -0,0 +1,26 @@
+package member;
+
+public class IdSearchDTO {
+	private String id;
+	private String user_name;
+	private String phone_num;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPhone_num() {
+		return phone_num;
+	}
+	public void setPhone_num(String phone_num) {
+		this.phone_num = phone_num;
+	}	
+}
